e97f1fc098135a08775c8f0b6285f63a077b27db
utigernils <utigernils@gmail.com>
2025-12-04 20:58:00 +0100

    fix; frontend path

--- ApiServices/frontend_service.py
+++ ApiServices/frontend_service.py
@@ -9,7 +9,7 @@ from pathlib import Path
 class FrontendService:
     """Handles serving the web static files."""
     
-    def __init__(self, frontend_path: str = "ApiServices/web"):
+    def __init__(self, frontend_path: str = "./web/"):
         self.frontend_path = Path(frontend_path)
     
     def mount_frontend(self, app: FastAPI):
